6708518c49e466d5af0f8c5de9d32856f11b098b
aadamcz <33779500+aadamcz@users.noreply.github.com>
2017-11-24 21:39:00 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 //Ask user about name
+/*
+  But 
+  first
+  I will
+  put a nice block comment
+*/
 var name = prompt("Enter your name");
 //alert("Hello," + name);
 //Check output in dev Tools
+/*
+  Another very very nice block comment
+*/
 console.log("Hello, " + name);
